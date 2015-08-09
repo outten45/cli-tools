e13c47935e98bde8470874306b27cc577e7d76e1
Richard (jr) Outten <outtenr@gmail.com>
2015-08-08 20:39:53 -0400

    Remove stdout
    Remove some stdout statements and other cleanup.

--- cmd/glab/main.go
+++ cmd/glab/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,9 +10,7 @@ import (
 )
 
 func projectList(args []string) {
-
-	fmt.Printf("args: %v\n", args)
-	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "GLAB", flag.PanicOnError)
+	fs := flag.NewFlagSetWithEnvPrefix(args[0], "GLAB", flag.PanicOnError)
 
 	host := fs.String("host", "", "name of the gitlab host")
 	apiPath := fs.String("apipath", "", "api path on the gitlab host")
@@ -39,7 +36,6 @@ func main() {
 	}
 
 	args := append(os.Args[:1], os.Args[2:]...)
-	// args := os.Args[2:]
 
 	switch mode {
 	case "projects":
