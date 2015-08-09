a2de5c538db07ca25409f8cceb229cca55e45ca9
Richard (jr) Outten <outtenr@gmail.com>
2015-08-08 22:44:56 -0400

    Add project fetch and filtering
    Fetch projects from gitlab and offer the option to filter based on a regex
    passed as an argument.

--- cmd/glab/main.go
+++ cmd/glab/main.go
@@ -4,40 +4,74 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/namsral/flag"
 	gogitlab "github.com/plouc/go-gitlab-client"
 )
 
-func projectList(args []string) {
+type context struct {
+	Gitlab *gogitlab.Gitlab
+	Match  string
+}
+
+func client(args []string) context {
 	fs := flag.NewFlagSetWithEnvPrefix(args[0], "GLAB", flag.PanicOnError)
 
 	host := fs.String("host", "", "name of the gitlab host")
 	apiPath := fs.String("apipath", "", "api path on the gitlab host")
 	token := fs.String("token", "", "token for gitlab")
+	match := fs.String("r", "", "regular expression to match the projects")
 
 	if err := fs.Parse(args[1:]); err != nil {
-		fmt.Errorf("error: %s\n", err)
+		log.Fatalf("Error parsing project options: %s\n", err)
 	}
 
-	gitlab := gogitlab.NewGitlab(*host, *apiPath, *token)
-	fmt.Printf("gitlab: %+v \n", *gitlab)
+	return context{
+		Gitlab: gogitlab.NewGitlab(*host, *apiPath, *token),
+		Match:  *match}
+}
 
+func filterProjects(projects []*gogitlab.Project, match string) []gogitlab.Project {
+	var fp []gogitlab.Project
+	r, _ := regexp.Compile(fmt.Sprintf("(?i)%s", match))
+
+	for _, project := range projects {
+		if r.MatchString(project.Path) {
+			fp = append(fp, *project)
+		}
+	}
+	return fp
+}
+
+func projectList(args []string) {
+	c := client(args)
+	// fmt.Printf("context: %+v \n", *c.Gitlab)
+	projects, err := c.Gitlab.Projects()
+	if err != nil {
+		log.Fatalf("Error fetching projects: %s\n", err)
+	}
+
+	fp := filterProjects(projects, c.Match)
+
+	fmt.Printf("\n\tprojects found: %d\n\n", len(fp))
+	for c, project := range fp {
+		fmt.Printf("%d. %s: %s\n", (c + 1), project.Name, project.PathWithNamespace)
+		fmt.Printf("  > %s\n\n", project.SshRepoUrl)
+	}
+	fmt.Println(" ")
 }
 
 func main() {
 
-	mode := ""
+	subcommand := ""
+	var args []string
 	if len(os.Args) > 1 {
-		mode = os.Args[1]
-	} else {
-		fmt.Println("Used needs to include a subcommand.")
-		return
+		subcommand = os.Args[1]
+		args = append(os.Args[:1], os.Args[2:]...)
 	}
 
-	args := append(os.Args[:1], os.Args[2:]...)
-
-	switch mode {
+	switch subcommand {
 	case "projects":
 		projectList(args)
 	// case "replay":
