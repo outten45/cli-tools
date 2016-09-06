d40d8effa47ea1419bf0552931db81d3190d642b
Richard (jr) Outten <outtenr@gmail.com>
2016-09-05 20:24:16 -0400

    Setup basic structure for reporting
    Arrange results so they can be used and displayed later. The output is
    pretty basic now, but would like to use html/template next to create an
    html page.

--- cmd/jresults/jresults.go
+++ cmd/jresults/jresults.go
@@ -6,8 +6,41 @@ import (
 	"os"
 
 	"github.com/gocarina/gocsv"
+	"github.com/montanaflynn/stats"
 	"github.com/namsral/flag"
 )
+
+func main() {
+	// handle the arguments
+	args := os.Args
+	fs := flag.NewFlagSetWithEnvPrefix(args[0], "GLAB", flag.ExitOnError)
+	file := fs.String("csv", "", "csv file with the results to parse")
+	fs.Parse(args[1:])
+
+	if *file == "" {
+		log.Fatal("csv file name not provided.")
+	}
+
+	results := getResults(file)
+
+	fmt.Printf("%+v\n", results[0])
+
+	d := getStats(results)
+	// v, _ := stats.Mean(d.Total)
+	// fmt.Printf("data: %+v\n------\n%+v\n", d, v)
+	// fmt.Printf("data: %+v\n\n", d.Pages["Home Page"])
+	printResults(d)
+}
+
+func printResults(data jstats) {
+	for k, v := range data.Pages {
+		mn, _ := stats.Mean(v)
+		fmt.Printf("%s: \t %f\n", k, mn)
+	}
+
+	t, _ := stats.Mean(data.Total)
+	fmt.Printf("Total Mean: %f\n", t)
+}
 
 func getResults(file *string) []*Result {
 	f, err := os.Open(*file)
@@ -22,18 +55,24 @@ func getResults(file *string) []*Result {
 	return results
 }
 
-func main() {
-	args := os.Args
-	fs := flag.NewFlagSetWithEnvPrefix(args[0], "GLAB", flag.ExitOnError)
-	file := fs.String("csv", "", "csv file with the results to parse")
-	fs.Parse(args[1:])
+func getStats(results []*Result) jstats {
+	pagesRaw := make(map[string][]int)
+	totalRaw := make([]int, len(results))
 
-	if *file == "" {
-		log.Fatal("csv file name not provided.")
+	for i, r := range results {
+		if _, ok := pagesRaw[r.Label]; !ok {
+			pagesRaw[r.Label] = make([]int, 0)
+		}
+		pagesRaw[r.Label] = append(pagesRaw[r.Label], r.Elapsed)
+		totalRaw[i] = r.Elapsed
 	}
 
-	results := getResults(file)
-	fmt.Printf("%+v\n", results[0])
+	pages := make(map[string]stats.Float64Data)
+	for k, v := range pagesRaw {
+		pages[k] = stats.LoadRawData(v)
+	}
+	total := stats.LoadRawData(totalRaw)
+	return jstats{Pages: pages, Total: total}
 }
 
 type Result struct {
@@ -56,3 +95,8 @@ type Result struct {
 // success - true or false
 // bytes - number of bytes in the sample
 // latency - time to first response
+
+type jstats struct {
+	Pages map[string]stats.Float64Data
+	Total stats.Float64Data
+}
